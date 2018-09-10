5dd14070bbb2d302642f676cd227c7efc9367ceb
Joey <mcnattj7@students.rowan.edu>
2018-09-10 17:26:21 -0400

    Updated math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,1 +1,65 @@
+#include math.h
 
+/*
+Author: Joey McNatt
+Created 9/10/18
+Updated 9/10/18
+This program works as a simple integer calculator capable of addition, subtraction, multiplication, division,
+modulus, left and right shifts, and bitwise functions AND, OR, XOR, and inversion.
+*/
+
+int math(int num1, int num2, char Operator){
+
+switch (Operator){
+
+case '+':{
+	return num1 + num2;
+}
+
+case '-':{
+	return num1 - num2;
+}
+
+case '*':{
+	return num1 * num2;
+}
+
+case '/':{
+	return num1 / num2;
+}
+
+case '%':{
+	return num1 % num2;
+}
+
+case '<':{
+	return num1 << num2;
+}
+
+case '>':{
+	return num1 >> num2;
+}
+
+case '&':{
+	return num1 & num2;
+}
+
+case '|':{
+	return num1 | num2;
+}
+
+case '^':{
+	return num1 ^ num2;
+}
+
+case '~':{
+	return ~num1;
+}
+
+
+default:
+return 0;
+
+}
+
+}
