8e0b8da1bfa3a92a5bd54d36cfab17400dcbb6d4
JoeyMcNatt <36242721+JoeyMcNatt@users.noreply.github.com>
2018-09-10 17:31:53 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -6,6 +6,7 @@ Created 9/10/18
 Updated 9/10/18
 This program works as a simple integer calculator capable of addition, subtraction, multiplication, division,
 modulus, left and right shifts, and bitwise functions AND, OR, XOR, and inversion.
+Accepts integer inputs and outputs an integer. Illegal operators always output 0.
 */
 
 int math(int num1, int num2, char Operator){
